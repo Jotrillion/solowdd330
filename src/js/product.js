6426bba11bcd853c0c03fea53d96df55bce5a276
hvelarderiquelme <hvelardeab@gmail.com>
2026-01-09 13:37:38 -0700

    solved cart bug

--- src/js/product.js
+++ src/js/product.js
@@ -1,13 +1,19 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart", ""); // get cart array of items from local storage if null set to empty array
-  //cartItems.push();
-  setLocalStorage("so-cart", cartItems);
+  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
+  console.log("type of cartItems:", typeof cartItems);
+  console.log("Is it an array?", Array.isArray(cartItems));
+  cartItems.push(product);
+  setLocalStorage("so-cart",cartItems);
+
+  console.log("cartItems:", cartItems);
+  // console.log("testArray", testArray);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
