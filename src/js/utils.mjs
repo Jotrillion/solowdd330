7c834f607da89448832bf0a864b048314128d554
LaceySana <laceysanabria@gmail.com>
2026-01-13 11:52:39 -0700

    Undoing my changes in utils.js

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -11,9 +11,7 @@ export function getLocalStorage(key) {
 }
 // save data to local storage
 export function setLocalStorage(key, data) {
-  const cart = getLocalStorage(key) || [];
-  cart.push(data);
-  localStorage.setItem(key, JSON.stringify(cart));
+  localStorage.setItem(key, JSON.stringify(data));
 }
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
