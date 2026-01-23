3adc3253853d24668db36206cc1f76b7e82e19d4
jmalan210 <>
2026-01-22 23:05:15 -0700

    each category displaying when icons are clicked (finished through step 5).

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,12 +1,14 @@
-import { loadHeaderFooter } from "./utils.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
-const dataSource = new ProductData("tents");
+const category = getParam("category");
+
+const dataSource = new ProductData();
 
 const element = document.querySelector(".product-list");
 
-const productList = new ProductList("Tents", dataSource, element);
+const productList = new ProductList(category, dataSource, element);
 
 //
 
